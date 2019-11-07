3748dc14c8a462ec1407b8aae39b022c4acefe95
kevinalvian <55798459+kevinalvian@users.noreply.github.com>
2019-11-07 22:56:16 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118053/latihan56/umurbarangantik/radio.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118053.latihan56.umurbarangantik;
+
+/**
+ *
+ * @author ACER
+ */
+public class radio extends barangantik {
+    private String name;
+
+    public radio(int umur) {
+        super(umur);
+    }
+
+    public String getName() {
+         System.out.println("Nama barang Antik : "+name+" AM");
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
